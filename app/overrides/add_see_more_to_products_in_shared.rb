ccdacdeac39eb404f7b06efc1e732ece4e3abcce
Quantumphilisp <quantumphilisp@gmail.com>
2019-03-25 11:31:41 -0400

    modif

--- app/overrides/add_see_more_to_products_in_shared.rb
+++ app/overrides/add_see_more_to_products_in_shared.rb
@@ -5,10 +5,3 @@
                        :text         =>"    <div class='button-outline centered no-margin-top load-more' id='more_products'>
         <%= link_to_next_page products, t('spree.product_list.see_more'), params: params, remote: true,  data: {disable_with: \"<i class='fa fa-refresh fa-spin'></i> ...\".html_safe } %>
       </div>")
-
-  Deface::Override.new(:virtual_path=>'spree/shared/_products',
-                     :name => 'add_see_more_to_products_in_shared',
-                     :insert_bottom =>"#products",
-                     :text         =>"    <div class='button-outline centered no-margin-top load-more' id='more_products'>
-      <%= link_to_next_page products, t('spree.product_list.see_more'), params: params, remote: true,  data: {disable_with: \"<i class='fa fa-refresh fa-spin'></i> ...\".html_safe } %>
-    </div>")
